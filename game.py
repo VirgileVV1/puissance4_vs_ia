3bbb04948f03fea5ca9fd5881e8c05ef86e5be53
Virgile <virgilevallvillellas@gmail.com>
2023-12-07 10:26:42 +0100

    Create class Game

---
+++ game.py
@@ -0,0 +1,29 @@
+import pygame
+
+class Game:
+    
+    def __init__(self):
+        self.power4 = []
+        self.initialize()
+        self.start_game()
+
+    def initialize(self):
+        for i in range(6):
+            temp = []
+            for j in range(7):
+                temp.append(0)
+            self.power4.append(temp)
+
+    def start_game(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((640, 455))
+        run = True
+
+        while run:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+            self.screen.fill((255, 255, 255))  # Remplir l'écran avec une couleur blanche
+            pygame.display.flip()
+
+        pygame.quit()
