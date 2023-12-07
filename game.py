22310f65aa97f86836c8c681740e697cc9e0f408
Matthieu PAYS <pays.matthieuic@gmail.com>
2023-12-07 11:02:45 +0100

    Faire la grille de jeu

--- game.py
+++ game.py
@@ -6,15 +6,19 @@ class Game:
 
     def __init__(self):
         self.power4 = []
+        self.rects = []
         self.initialize()
         self.start_game()
 
     def initialize(self):
         for i in range(6):
             temp = []
+            rect = []
             for j in range(7):
                 temp.append(0)
+                rect.append(None)
             self.power4.append(temp)
+            self.rects.append(rect)
 
     def start_game(self):
         pygame.init()
@@ -26,6 +30,10 @@ class Game:
                 if event.type == pygame.QUIT:
                     run = False
             self.screen.fill((255, 255, 255))  # Remplir l'écran avec une couleur blanche
+            for ligne in range(6):
+                for colonne in range(7):
+                    self.rects[ligne][colonne] =  pygame.draw.rect(self.screen, (0, 0, 0), (16 + colonne * 64, 32 + ligne * 64, 64, 64), 2)
+            
             pygame.display.flip()
 
         pygame.quit()
