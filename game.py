041b4d786ab7d62d70dc45c6863a92e6d163b994
Virgile <virgilevallvillellas@gmail.com>
2023-12-07 13:44:33 +0100

    fix rounds

--- game.py
+++ game.py
@@ -2,7 +2,7 @@ import pygame
 from timer import Timer
 
 CELL_SIZE = 64
-OFFSET_TOP = 16
+OFFSET_TOP = 48
 OFFSET_LEFT = 32
 PIECE_SIZE = 25 # x2
 
@@ -27,14 +27,14 @@ class Game:
             self.power4.append(temp)
             self.rects.append(rect)
 
-    def add_piece(self, index_column_to_add):
+    def add_piece(self, index_column_to_add, value):
         index_row = None
         for i in range(len(self.rects)-1, -1, -1):
             if (self.power4[i][index_column_to_add] == 0):
                 index_row = i
                 break
         if index_row is not None:
-            self.power4[index_row][index_column_to_add] = 1
+            self.power4[index_row][index_column_to_add] = value
             return True
         return False
 
@@ -46,18 +46,21 @@ class Game:
                     for row in self.rects:
                         for col, rect in enumerate(row):
                             if (rect.collidepoint(event.pos)):
-                                if self.add_piece(col):
+                                if self.add_piece(col, 1):
                                     self.round += 1
 
             if event.button == 3:
                 print("left clic")
 
-    def print_piece(self, row, col):
+    def print_piece(self, row, col, color):
         #self.rects[row][col] =  pygame.draw.rect(self.screen, (0, 0, 0), (16 + col * CELL_SIZE, 32 + row * CELL_SIZE, CELL_SIZE, CELL_SIZE), 2)
         x = col * CELL_SIZE + OFFSET_LEFT + CELL_SIZE/2
         y = row * CELL_SIZE + OFFSET_TOP + CELL_SIZE/2
-        pygame.draw.circle(self.screen, (255,0,0), (x,y), PIECE_SIZE, PIECE_SIZE)
+        pygame.draw.circle(self.screen,color, (x,y), PIECE_SIZE, PIECE_SIZE)
 
+    def ai_round(self):
+        col = 0
+        return col
 
     def start_game(self):
         pygame.init()
@@ -66,15 +69,18 @@ class Game:
         self.screen = pygame.display.set_mode((640, 455))
         run = True
 
-        old_round = self.round
-
         while run:            
-            self.screen.fill((255, 255, 255))  # Remplir l'écran avec une couleur blanche
+            self.screen.fill((255, 255, 255))  
 
-            if old_round != self.round:
-                old_round = self.round
+       
+            if (self.round % 2) == 1:
                 # IA ici
-        
+                col = self.ai_round()
+                
+                if (self.add_piece(col, 2)):
+                    self.round += 1
+
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
@@ -83,8 +89,11 @@ class Game:
             for row in range(self.LINES):
                 for col in range(self.COLUMN):
                     self.rects[row][col] =  pygame.draw.rect(self.screen, (0, 0, 0), (OFFSET_LEFT + col * CELL_SIZE, OFFSET_TOP + row * CELL_SIZE, CELL_SIZE, CELL_SIZE), 2)
-                    if self.power4[row][col] != 0:
-                        self.print_piece(row, col)
+                    if self.power4[row][col] == 1:
+                        self.print_piece(row, col, (255,0,0))
+                    if self.power4[row][col] == 2:
+                        self.print_piece(row, col, (0,255,0))
+
             timer.update(pygame.time.get_ticks())
             
             font = pygame.font.SysFont('Arial', 16)
