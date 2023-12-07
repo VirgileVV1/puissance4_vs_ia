4f45995029619e93e88811150d7ff515aa80f3a6
Matthieu PAYS <pays.matthieuic@gmail.com>
2023-12-07 11:08:05 +0100

    Utilisation de constante pour les lignes et colonnes

--- game.py
+++ game.py
@@ -5,16 +5,18 @@ import pygame
 class Game:
 
     def __init__(self):
+        self.LINES = 6
+        self.COLUMN = 7
         self.power4 = []
         self.rects = []
         self.initialize()
         self.start_game()
 
     def initialize(self):
-        for i in range(6):
+        for i in range(self.LINES):
             temp = []
             rect = []
-            for j in range(7):
+            for j in range(self.COLUMN):
                 temp.append(0)
                 rect.append(None)
             self.power4.append(temp)
@@ -30,8 +32,8 @@ class Game:
                 if event.type == pygame.QUIT:
                     run = False
             self.screen.fill((255, 255, 255))  # Remplir l'écran avec une couleur blanche
-            for ligne in range(6):
-                for colonne in range(7):
+            for ligne in range(self.LINES):
+                for colonne in range(self.COLUMN):
                     self.rects[ligne][colonne] =  pygame.draw.rect(self.screen, (0, 0, 0), (16 + colonne * 64, 32 + ligne * 64, 64, 64), 2)
             
             pygame.display.flip()
